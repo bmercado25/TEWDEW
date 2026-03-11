8cce14971e95a5757e9f7db6294c39ea533cb982
bmercado25 <breemercado19@gmail.com>
2026-03-11 16:54:21 -0400

    please help

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const express = require('express');
 const app = require('./api/index.js');
 
 const PORT = process.env.PORT || 3000;
@@ -5,3 +7,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
+
+module.exports = app;
